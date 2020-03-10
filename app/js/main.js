668cdfc99813c7d8d97b56282eca412e27d4f471
Ganna-S <gannashutenko@gmail.com>
2020-03-10 23:59:45 +0200

    adaptive added

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,28 @@ $(function () {
         slidesToScroll: 2,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        ]
     });
+
     $('.customers__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         arrows: false,
     });
+
+    $('.menu__btn').on('click', function () {
+        $('.header__menu').slideToggle();
+    });
+
 });
